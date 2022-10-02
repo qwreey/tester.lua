8a9cbd7eb20574aed406535285bc9ceb6878f012
console.log(cat == cute) <qwreey75@gmail.com>
2022-10-03 04:42:04 +0900

    Update waitForEnter.lua

--- libs/waitForEnter.lua
+++ libs/waitForEnter.lua
@@ -2,9 +2,21 @@ local module = {};
 
 function module:init(io,print)
     return function (q)
-        print(q or "[Enter for continue or Ctrl+C for cancel ...]");
-        io.write("Test > ");
-        return io.read();
+        print(q or "[Enter for continue or Ctrl+C for cancel ...] ");
+        local readed = io.read();
+        if not readed then
+            local err,pretty = pcall(require,"pretty-print");
+            if error then
+                print("NO INPUT PROVIDER AVAILABLE");
+                return;
+            end
+            local stdin = pretty.stdin;
+            stdin:tty_set_mode(0);
+            stdin:read_start(function(err,content)
+               --if content == ""
+            end);
+        end
+        return readed
     end;
 end
 
